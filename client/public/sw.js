82cbc708b9efc804522ea3f2b149158c879ad34f
swetha <94050914+swetha22p@users.noreply.github.com>
2023-11-29 14:46:23 +0530

    Add files via upload

---
+++ client/public/sw.js
@@ -0,0 +1,24 @@
+let cacheData="appV1";
+this.addEventListener("install" ,(event)=>{
+    event.waitUntil(
+        caches.open(cacheData).then((cache)=>{
+            cache.addAll([
+                  '/static/js/bundle.js',
+                  '/index.html',
+                  '/',
+                  '/about',
+                //   '/users',
+            ]);
+        })
+    );
+});
+
+this.addEventListener("fetch",(event)=>{
+    event.respondWith(
+        caches.match(event.request).then((resp)=>{
+            if(resp){
+                return resp
+            }
+        })
+    )
+})
--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,30 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Home';
+import About from './About';
+import Users from './Users';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
+      <Router>
+        <Navbar bg="primary" data-bs-theme="dark">
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/users">Users</Nav.Link>
           </Nav>
-        </Container>
-      </Navbar>
-      
+        </Navbar>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
